3075206eff298f08d0e22690c205d0b87b4e6bf8
rastokavon <kavon.rastislav@gmail.com>
2020-05-06 17:18:36 +0200

    Logging started

--- src/database/CreateEntity.java
+++ src/database/CreateEntity.java
@@ -3,13 +3,17 @@ package database;
 import controllers.MainController;
 import org.hibernate.*;
 import org.hibernate.cfg.Configuration;
+import sun.rmi.runtime.Log;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class CreateEntity {
+    private static final Logger LOG = Logger.getLogger(CreateEntity.class.getName());
 
     public static Company getCompanyFromID(Integer companyID) {
         Session session = CreateDatabase.getSession();
@@ -17,8 +21,8 @@ public class CreateEntity {
         CriteriaBuilder cb = session.getCriteriaBuilder();
         CriteriaQuery<Company> cr = cb.createQuery(Company.class);
         Root<Company> root = cr.from(Company.class);
-        cr.select(root);
-        cr.select(root).where(cb.gt(root.get("id"), companyID));
+
+        cr.select(root).where(cb.gt(root.get("id"), companyID - 1));
 
         Query<Company> query = session.createQuery(cr);
         query.setMaxResults(1);
@@ -26,22 +30,31 @@ public class CreateEntity {
         session.getTransaction().commit();
         session.close();
 
-        System.out.println(results.isEmpty());
+        if (results.isEmpty()) {
+            LOG.log(Level.WARNING, "Take cislo miestnosti neexistuje");
+            return new Company();
+        }
 
         return results.get(0);
+
     }
 
     public static void createPerson(String firstName, String lastName, String username,
                                     String password, String mail, String phoneNumber, int company) {
-        Session session = CreateDatabase.getSession();
-        Transaction t = session.beginTransaction();
+        try {
+            Session session = CreateDatabase.getSession();
 
-        session.save(new Person(firstName, lastName, username, password, mail, phoneNumber, getCompanyFromID(company)));
+            Transaction t = session.beginTransaction();
 
-        t.commit();
-        session.close();
+            session.save(new Person(firstName, lastName, username, password, mail, phoneNumber, getCompanyFromID(company)));
 
-        System.out.println("Successfully saved new person...");
+            t.commit();
+            session.close();
+
+            System.out.println("Successfully saved new person...");
+        } catch(Exception e) {
+            LOG.log(Level.SEVERE, "Nepodarilo sa pridat zaznam");
+        }
     }
     public static void createCompany(String name, String street, String city,
                                      String country, String postalCode, String mail, String phoneNumber) {
