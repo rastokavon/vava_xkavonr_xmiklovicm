3162bd3c98ab627efd8076a20235fd55e5671771
rastokavon <kavon.rastislav@gmail.com>
2020-05-23 12:55:36 +0200

    Comment of TableAllPosts

--- src/tables/TableAllPosts.java
+++ src/tables/TableAllPosts.java
@@ -1,5 +1,13 @@
 package tables;
 
+/**
+ * Trieda, ktora obsahuje atributy, ktorymi sa naplnaju tabulky, konkretne tabulky otvoreneho prispevku,
+ * kde su jednotlive komentare v tabulke, ale taktiez tabulka vsetkych prispevkov v hlavnom menu pouzivatela
+ * po prihlaseni alebo v okno rozkliknuteho prihlaseneho pouzivatela
+ *
+ * Atributmi su 2 stlce v tabulke: v prvom je nadpis prispevku (resp. text komentaru), v druhom je datum a meno
+ * pouzivatela, ktory komentar pridal.
+ */
 public class TableAllPosts {
     private String title;
     private String dateName;
